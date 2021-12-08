681623ac4a0f1c64f9d05d80938dff218d530c13
n7560125 <95419045+n7560125@users.noreply.github.com>
2021-12-08 14:40:23 +0800

    add CollisionAvoid1 comment.

--- Assets/Collision/CollisionAvoid1.cs
+++ Assets/Collision/CollisionAvoid1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Check Distance in range.
+/// <summary>
+/// 以obj作為圓心，並以probe長度作為半徑畫圓(gizmos)，在圓內者判斷為潛在障礙物(威脅較大);
+/// </summary>
 public class CollisionAvoid1
 {
     static public bool CheckObstacleInRange(AIData data)
     {
+        //宣告障礙物list;
         List<Obstacle> m_AvoidTargets = Main.m_Instance.GetObstacles();
         Transform ct = data.m_Go.transform;
         Vector3 cPos = ct.position;
@@ -14,12 +18,14 @@ public class CollisionAvoid1
         Vector3 vec;
         float fDist = 0.0f;
         int iCount = m_AvoidTargets.Count;
+        //對每個障礙物做威脅(潛在)判定
         for(int i = 0; i < iCount; i++)
         {
             //m_AvoidTargets[i]
             vec = m_AvoidTargets[i].transform.position - cPos;
             vec.y = 0.0f;
             fDist = vec.magnitude;
+            //距離半徑圓外的障礙物劃分為outside;
             if (fDist > data.m_fProbeLength + m_AvoidTargets[i].m_fRadius)
             {
                 m_AvoidTargets[i].m_eState = Obstacle.eState.OUTSIDE_TEST;
